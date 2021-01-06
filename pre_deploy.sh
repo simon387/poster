8aca71177e3c1c54a227a9d10e24a57b242cb1c9
simone celia <simone.celia@thinkopen.it>
2021-01-06 23:27:30 +0100

    added_pre_deploy_script

---
+++ pre_deploy.sh
@@ -0,0 +1,20 @@
+rm -rf /home/scelia/Downloads/poster/
+
+mkdir -p /home/scelia/Downloads/poster
+
+cp -R . /home/scelia/Downloads/poster/
+
+rm -rf /home/scelia/Downloads/poster/dump
+rm -rf /home/scelia/Downloads/poster/http
+rm -rf /home/scelia/Downloads/poster/.git
+rm -rf /home/scelia/Downloads/poster/.gitignore
+rm -rf /home/scelia/Downloads/poster/.idea
+rm -rf /home/scelia/Downloads/poster/deploy_localhost.sh
+rm -rf /home/scelia/Downloads/poster/LICENSE
+rm -rf /home/scelia/Downloads/poster/pre_deploy.sh
+rm -rf /home/scelia/Downloads/poster/README.md
+rm -rf /home/scelia/Downloads/poster/todo.md
+
+rm -rf /home/scelia/Downloads/poster/be/config/database.php
+
+rm -rf /home/scelia/Downloads/poster/fe/vendor
