319d089cf054e7966ba1a91fcc6dd07a1d7fdaa8
simone celia <simone.celia@thinkopen.it>
2021-01-06 11:54:44 +0100

    end_fe

--- fe/js/post/post.js
+++ fe/js/post/post.js
@@ -1,15 +1,18 @@
 
 $(document).ready(function () {
-
-	getDatatableDataBySupplierAndPeriod($(this).attr("data-text"), period["id"]);
+	$("#btn-add-new-post").on("click", function () {
+		$('#newPostModal').modal('show');
+	});
+	$("#btn-add-post").on("click", function () {
+		addNewPost();
+	});
+	getDatatableData();
 });
-
-
 
 const dataTablePost = $('#dataTablePost').DataTable({
 	paging: true,
 	searching: true,
-	autoWidth: false,
+	autoWidth: true,
 	columnDefs: [
 		{"width": "10%", orderable: false, targets: [0]},
 		{"width": "10%", orderable: false, targets: [1]},
@@ -18,88 +21,56 @@ const dataTablePost = $('#dataTablePost').DataTable({
 	aaSorting: [],
 });
 
-function getSuppliers() {
-	$.ajax({
-		type: 'GET',
-		url: rest + "supplier/readAll.php",
-		processData: false,
-		contentType: false,
-		success: function (data) {
-			const array = JSON.parse(data).list;
-			let innerHTML = "";
-			for (let i = 1; i < array.length; i++) {
-				let active = "";
-				if (i === 1) {
-					active = "active";
-					document.getElementById("supplier-selected").innerHTML = array[i]["name"].toUpperCase();
-				}
-				innerHTML += '<li class="page-item ' + active + '" data-text="' + array[i]["id"] +
-					'"><a class="page-link" href="#">' + array[i]["name"].toUpperCase() + '</a></li>';
-			}
-
-			document.getElementById("main-paginator").innerHTML = innerHTML;
-
-			$(".page-item").on("click", function () {
-				removeActiveClassFromSupplier();
-				$(this).addClass('active');
-				document.getElementById("supplier-selected").innerHTML = this.innerText;
-				getDatatableDataBySupplierAndPeriod($(this).attr("data-text"), period["id"]);
-			});
-		}
-	});
-}
-
-function removeActiveClassFromSupplier() {
-	const elems = document.querySelectorAll(".page-item");
-	[].forEach.call(elems, function (el) {
-		el.classList.remove("active");
-	});
-}
-
-function getDatatableDataBySupplierAndPeriod(supplierID, periodID) {
+function getDatatableData() {
 	blockScreen();
 	$.ajax({
 		type: "GET",
-		url: rest + "product/read.php?supplier_id=" + supplierID + "&period_id=" + periodID,
+		url: contextPath + "be/post/readAll.php",
 		success: function (data) {
-			renderTableDashboard(dataTableDashboard, data);
+			renderTable(data);
 		},
 		error: function () {
 			console.clear();
 			unblockScreen();
-			dataTableDashboard.clear();
-			dataTableDashboard.draw();
+			dataTablePost.clear();
+			dataTablePost.draw();
 		}
 	});
 }
 
-function renderTableDashboard(dataTable, data, isFromSearch = false) {
-	dataTable.clear();
-	const array = isFromSearch ? data.list : JSON.parse(data).list;
-	const disabledDeposit = user["role"] === "admin" ? "" : "disabled";
-	const disabledOf0 = user["role"] === "comarea" ? "disabled" : "";
-	const disabledOf1 = user["role"] === "placa" ? "disabled" : "";
+function renderTable(data) {
+	dataTablePost.clear();
+	const array = JSON.parse(data).list;
 	$.each(array, function (ind, o) {
 		const id = o["id"];
-		const name = isFromSearch ? o["supplier"].toUpperCase() + " - <strong>" + o["name"] + "</strong>" : o["name"];
-		const unit = null === o["unit"] ? "" : o["unit"];
-		const note = null === o["note"] ? "" : o["note"];
-		const deposit = null === o["deposit"] ? "" : o["deposit"];
-		const outflow0 = null === o["outflow0"] ? "" : o["outflow0"];
-		const outflow1 = null === o["outflow1"] ? "" : o["outflow1"];
-		const left = calcFlow(deposit, outflow0, outflow1);
-		const lastOperation = null === o["lastOperation"] ? "" : o["lastOperation"];
-		dataTable.row.add([
-			name,
-			unit,
-			note,
-			'<input ' + disabledDeposit + ' onchange="updateProduct(' + id + ', 0)" type="number" min="0" value="' + deposit + '" class="form-control" id="deposit-' + id + '">',
-			'<input ' + disabledOf0 + ' onchange="updateProduct(' + id + ', 1)" type="number" min="0" value="' + outflow0 + '" class="form-control" id="outflow0-' + id + '">',
-			'<input ' + disabledOf1 + ' onchange="updateProduct(' + id + ', 2)" type="number" min="0" value="' + outflow1 + '" class="form-control" id="outflow1-' + id + '">',
-			'<input disabled type="number" value="' + left + '" class="form-control" id="left-' + id + '">',
-			'<div id="lastOperation-' + id + '">' + lastOperation + '</div>',
+		const timestamp = o["timestamp"];
+		const text = o["text"];
+		dataTablePost.row.add([
+			id,
+			timestamp,
+			text,
 		]);
 	});
-	dataTable.draw();
+	dataTablePost.draw();
 	unblockScreen();
+}
+
+function addNewPost() {
+	const text = document.getElementById("text-modal-input").value;
+	document.getElementById("text-modal-input").value = "";
+	blockScreen();
+	$.ajax({
+		type: "PUT",
+		url: contextPath + "be/post/create.php",
+		contentType: "application/json; charset=utf-8",
+		processData: false,
+		dataType: "json",
+		data: JSON.stringify({
+			text,
+		}),
+		success: function () {
+			location.reload();
+			unblockScreen();
+		},
+	});
 }
