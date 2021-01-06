55846d16677d5306dab278f5847379f3802a5bc1
simone celia <simone.celia@thinkopen.it>
2021-01-06 23:08:48 +0100

    upd_dump

--- dump/dump.sql
+++ dump/dump.sql
@@ -4,5 +4,5 @@ create table post
 (
     id        int auto_increment primary key,
     timestamp timestamp default current_timestamp() not null on update current_timestamp(),
-    text      varchar(1024)                         null
+    text      text(16384) null
 );
