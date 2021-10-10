418c5cf80a7b84f44e158c3be251a823f36c1397
simone celia <simone.celia@thinkopen.it>
2021-10-11 00:27:38 +0200

    config.php

--- README.md
+++ README.md
@@ -1,9 +1,20 @@
 # poster
-
 + mariaDB
 + PHP 7.4.13 (cli) (built: Nov 24 2020 10:03:34) ( NTS )
 
-## Links
+## config.php example
+```php
+<?php
 
+class Config
+{
+	static $host = '';
+	static $db_name = '';
+	static $username  = '';
+	static $password  = '';
+}
+```
+
+## Links
 + https://startbootstrap.com/theme/sb-admin-2
 + https://www.simonecelia.it/poster/
--- be/config/database.php
+++ be/config/database.php
@@ -3,17 +3,14 @@
 
 class Database
 {
-	private $host = "172.17.0.2"; //it's a locker instance
-	private $db_name = "poster";
-	private $username = "root"; //lol fake, is not the production one
-	private $password = "root";
 	public $conn;
 
 	public function getConnection()
 	{
+		$configs = include('config.php');
 		$this->conn = null;
 		try {
-			$this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+			$this->conn = new PDO("mysql:host=" . $configs::$host . ";dbname=" . $configs::$db_name, $configs::$username, $configs::$password);
 			$this->conn->exec("set names utf8");
 			$this->conn->exec("SET GLOBAL time_zone='Europe/Madrid");
 		} catch (PDOException $exception) {
