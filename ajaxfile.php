5d9cac9851aac42d3b362bea850429a635330790
Simone Celia <simone.celia@simonecelia.it>
2022-06-25 15:32:53 +0200

    1.3.0

---
+++ ajaxfile.php
@@ -0,0 +1,24 @@
+<?php
+
+// file name
+$filename = $_FILES['file']['name'];
+
+// Location
+$location = 'upload/' . $filename;
+
+// file extension
+$file_extension = pathinfo($location, PATHINFO_EXTENSION);
+$file_extension = strtolower($file_extension);
+
+// Valid file extensions
+$image_ext = array("jpg", "png", "jpeg", "gif", "svg", "txt", "java", "zip", "rar", "log", "xml");
+
+$response = 0;
+if (in_array($file_extension, $image_ext)) {
+	// Upload file
+	if (move_uploaded_file($_FILES['file']['tmp_name'], $location)) {
+		$response = $location;
+	}
+}
+
+echo $response;
