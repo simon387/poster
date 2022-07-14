db10ba3a25a51c2bbf5ad653f6367bd8456281b1
Simone Celia <simone.celia@simonecelia.it>
2022-07-14 19:43:40 +0200

    1.4.4

--- dump/create.sql
+++ dump/create.sql
@@ -6,6 +6,6 @@ create table post
 (
     id        int auto_increment primary key,
     timestamp timestamp default current_timestamp() not null on update current_timestamp(),
-    text      text(16384) null,
+    text      mediumtext null,
     deleted   tinyint(1) default 0                   not null
 );
