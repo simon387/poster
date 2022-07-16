09cc335bae7d5261d7d07aee5ddc1fa2b7b364c3
simone celia <simone.celia@eng.it>
2022-07-16 09:12:54 +0200

    1.4.6

--- dump/create.sql
+++ dump/create.sql
@@ -5,7 +5,7 @@ use poster;
 create table post
 (
     id        int auto_increment primary key,
-    timestamp timestamp default current_timestamp() not null on update current_timestamp(),
-    text      mediumtext null,
+    timestamp timestamp  default current_timestamp() not null on update current_timestamp(),
+    text      mediumtext                             null,
     deleted   tinyint(1) default 0                   not null
 );
