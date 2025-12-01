34f7ab68844d1af2d499cd5f5366f73154e693c2
Simone Celia <simone.celia@simonecelia.it>
2025-12-01 11:24:16 +0100

    1.4.31

--- be/models/SSP.php
+++ be/models/SSP.php
@@ -423,10 +423,13 @@ class SSP
 	{
 		try {
 			$db = @new PDO(
-				"mysql:host={$sql_details['host']};dbname={$sql_details['db']}",
+				"mysql:host={$sql_details['host']};dbname={$sql_details['db']};charset=utf8mb4",
 				$sql_details['user'],
 				$sql_details['pass'],
-				array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)
+				array(
+					PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+					PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4"
+				)
 			);
 		} catch (PDOException $e) {
 			self::fatal(
